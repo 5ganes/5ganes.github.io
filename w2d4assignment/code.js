c7b6e54b8c7e036c5e95e4ab983d70bcf4240ea7
Ganesh Khatri <kh6gnanesh@gmail.com>
2021-07-29 23:07:06 -0500

    w2d4-lab10 update

--- w2d4assignment/code.js
+++ w2d4assignment/code.js
@@ -1,12 +1,16 @@
-// filter function on String object to filter a string by removing a string
-String.prototype.filter = function(str){
-    var regex = new RegExp(" " + str, "g"); // RE to remove banned word str from all the places of string
-    return this.replace(regex, '');
+// problem 2 a) filter function on String object to filter a string by removing a string
+String.prototype.filter = function(...str){
+    let string = this;
+    for(let i = 0; i < str.length; i++){
+        var regex = new RegExp(str[i], "g");
+        string = string.replace(regex, '').replace(/  +/g, ' ');
+    }
+    return string;
 };
-// console.log("This not house is not nice!".filter('not'));
+// console.log("This not house bad is not nice!".filter('not', 'bad'));
 
 
-// bubble sort on Array object
+// problem 2 b) bubble sort on Array object
 Array.prototype.bubbleSort = function(){
     let temp;
     for(var i = 0; i < this.length; i++){
@@ -21,3 +25,36 @@ Array.prototype.bubbleSort = function(){
     return this;
 };
 // console.log([6,4,0, 3,-2,1].bubbleSort());
+
+
+// problem 2 c)
+var Person = function() {};
+Person.prototype.initialize = function(name, age){
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.describe = function(){
+    return this.name + ", " + this.age + " years old.";
+}
+var Student = function() {};
+Student.prototype = new Person();
+
+Student.prototype.learn = function(subject){
+    console.log(this.name + " just learned " + subject);
+}
+var me = new Student();
+me.initialize("John", 25);
+me.learn("Inheritance");
+
+// my code for teacher
+var Teacher = function(){
+    this.teach = function(subject){
+        return this.name + " is now teaching " + subject;    
+    }
+};
+Teacher.prototype = new Person();
+
+// this can also be used : teach is set outside constructor function like in Student
+// Teacher.prototype.teach = function(subject){ 
+//     return this.name + " is now teaching " + subject;
+// }
--- w2d4assignment/tests.js
+++ w2d4assignment/tests.js
@@ -2,7 +2,7 @@
 describe("filter", function () {
     it("takes an array of strings and filters a string removing those strings from array from the string",
         function () {
-            assert.equal("This house is nice!", "This house is not nice!".filter('not'));
+            assert.equal("This house is nice!", "This bad house is not nice!".filter('not', 'bad'));
         });
 });
 
@@ -11,5 +11,15 @@ describe("bubbleSort", function () {
     it("sorts an array of numbers",
         function () {
             assert.deepEqual([-2, 0, 1, 3, 4, 6], [6, 4, 0, 3, -2, 1].bubbleSort());
+        });
+});
+
+// problem 2 c)
+describe("teach", function () {
+    it("takes a subject and returns a string saying <teacher> is now teaching <subject>",
+        function () {
+            var teacher = new Teacher();
+            teacher.initialize("Michael Zijlstra", 40);
+            assert.deepEqual("Michael Zijlstra is now teaching CS401-WAP", teacher.teach("CS401-WAP"));
         });
 });
