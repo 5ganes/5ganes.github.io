4303c3d0ea51bfff57a5d6128893217a37366fb7
Ganesh Khatri <kh6gnanesh@gmail.com>
2021-08-05 22:05:43 -0500

    w3d4assignment

---
+++ w3d4assignment/problem3my_math.js
@@ -0,0 +1,11 @@
+module.exports.add = (x, y) => x + y;
+module.exports.subtract = (x, y) => x - y;
+module.exports.multiply = (x, y) => x * y;
+module.exports.divide = (x, y) => x / y;
+module.exports.pi = 3.14;
+
+// module.exports.add = add;
+// module.exports.subtract = subtract;
+// module.exports.multiply = multiply;
+// module.exports.divide = divide;
+// module.exports.pi = pi;
